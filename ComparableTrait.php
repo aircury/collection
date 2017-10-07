8faedcaec75b7e4cc878579a8ab5f8acbb266251
Jose Diaz <josemaria@diazangulo.com>
2017-10-07 14:07:12 +0100

    Added ComparableTrait

---
+++ ComparableTrait.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Aircury\Collection;
+
+trait ComparableTrait
+{
+    public function compareTo(int $comparisonMethod, ComparableInterface $element): bool
+    {
+        switch ($comparisonMethod) {
+            case ComparableInterface::IS_SAME_AS:
+                return $this->isSameAs($element);
+            case ComparableInterface::IS_IDENTICAL_TO:
+                return $this->isIdenticalTo($element);
+            default:
+                throw new \InvalidArgumentException('Unknown comparison method provided.');
+        }
+    }
+
+    public function isSameAs(ComparableInterface $element): bool
+    {
+        return $this == $element;
+    }
+
+    public function isIdenticalTo(ComparableInterface $element): bool
+    {
+        return $this === $element;
+    }
+}
--- Tests/Test/ComparableCar.php
+++ Tests/Test/ComparableCar.php
@@ -3,28 +3,9 @@
 namespace Aircury\Collection\Test;
 
 use Aircury\Collection\ComparableInterface;
+use Aircury\Collection\ComparableTrait;
 
 class ComparableCar extends Car implements ComparableInterface
 {
-    public function compareTo(int $comparisonMethod, ComparableInterface $element): bool
-    {
-        switch ($comparisonMethod) {
-            case ComparableInterface::IS_SAME_AS:
-                return $this->isSameAs($element);
-            case ComparableInterface::IS_IDENTICAL_TO:
-                return $this->isIdenticalTo($element);
-            default:
-                throw new \InvalidArgumentException('Unknown comparison method provided.');
-        }
-    }
-
-    public function isSameAs(ComparableInterface $element): bool
-    {
-        return $this == $element;
-    }
-
-    public function isIdenticalTo(ComparableInterface $element): bool
-    {
-        return $this === $element;
-    }
+    use ComparableTrait;
 }
